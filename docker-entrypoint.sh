7700b0df4bd6c4ae5f3bb7a44290a48c62d5c059
Adam Hall <adam.hall@aligent.com.au>
2022-07-07 15:48:42 +0930

    Added configuration schema object to validate dependabot configuration file Added support of loading a dependabot.yml file from the repository root Added support for specifying a list of packages and which versions to ignore Removed current references to assignees as it isn't supported by dependabot-core

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ if [ "${1:-}" = "bash" ]; then
   exec bash "$@"
 fi
 
+# Stash the current directory to the REPOSITORY_PATH env var (if it isn't already set)
+# In BB pipelines this will be where the local copy of the repository is mounted
+export REPOSITORY_PATH=${REPOSITORY_PATH:-${PWD}}
+
 pushd ${CODE_DIR} > /dev/null
 
 exec bundle exec ruby ./bitbucket-update-script.rb "$@"
